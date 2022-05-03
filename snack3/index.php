817c776f4b2b7ac9ed65e074c590da7f45f85baf
luciomelis <luciomelis1992@gmail.com>
2022-05-03 17:54:23 +0200

    Milestone 3 terminata

---
+++ snack3/index.php
@@ -0,0 +1,16 @@
+<?php
+// Snack 3
+// Creare un array con 15 numeri casuali, 
+// tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
+$arrayNumeri = [];
+for($i = 0; count($arrayNumeri) < 15; $i++){
+    $number = rand(1,100);
+    if(!in_array($number, $arrayNumeri)){
+        $arrayNumeri[]= $number;
+    }  
+}
+echo('<pre>');
+var_dump($arrayNumeri);
+echo('</pre>');
+?>
+
